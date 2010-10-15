19de2f45758a85342318c73f66aae84b04637be0
Michael Barton <mail@michaelbarton.me.uk>
2010-10-15 16:57:22 -0400

    Test sequence is passed to region when source used

--- lib/scaffolder.rb
+++ lib/scaffolder.rb
@@ -12,6 +12,10 @@ class Scaffolder < DelegateClass(Array)
 
     super(assembly.map do |entry|
       type, data = entry.keys.first, entry.values.first
+
+      # Source is the only keyword. Used to fetch sequence from fasta file.
+      data['raw_sequence'] = sequences[data['source']] if data['source']
+
       Scaffolder::Region[type].generate(data)
     end)
   end
--- test/test_scaffolder.rb
+++ test/test_scaffolder.rb
@@ -3,14 +3,18 @@ require 'helper'
 class TestScaffolder < Test::Unit::TestCase
   context Scaffolder do
     
+    Scaffolder::Region::Mock = Class.new(Scaffolder::Region)
+
     setup do
-      Scaffolder::Region::Mock = Class.new(Scaffolder::Region)
       @sequence = nil
-      Bio::FlatFile.expects(:auto).with(@sequence).returns({})
       @data = [{'mock' => Hash.new}]
     end
 
     context 'when instantiated' do
+
+      setup do
+        Bio::FlatFile.stubs(:auto).with(@sequence).returns({})
+      end
 
       should "fetch correct region class type" do
         Scaffolder::Region.expects(:'[]').with('mock').returns(Scaffolder::Region::Mock)
@@ -19,6 +23,22 @@ class TestScaffolder < Test::Unit::TestCase
 
       should "pass data to region object" do
         Scaffolder::Region::Mock.expects(:generate).with(@data.first['mock'])
+        Scaffolder.new(@data,@sequence)
+      end
+
+    end
+
+    context 'when instantiated with source tag' do
+
+      setup do
+        Bio::FlatFile.stubs(:auto).with(@sequence).returns([
+          stub(:definition => 'seq1', :seq => 'ATGC')])
+      end
+
+      should "should also pass raw_sequence from flat file" do
+        @data.first['mock']['source'] = 'seq1'
+        Scaffolder::Region::Mock.any_instance.expects(:source).with('seq1')
+        Scaffolder::Region::Mock.any_instance.expects(:raw_sequence).with('ATGC')
         Scaffolder.new(@data,@sequence)
       end
 
